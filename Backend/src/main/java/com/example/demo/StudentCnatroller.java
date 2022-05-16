195ece59128ed88edf6239b37382be429103f911
Neema <patelneema93@gmail.com>
2022-05-16 16:00:40 -0400

    able to add list of student

--- Backend/src/main/java/com/example/demo/StudentCnatroller.java
+++ Backend/src/main/java/com/example/demo/StudentCnatroller.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class StudentCnatroller {
@@ -10,5 +15,17 @@ public class StudentCnatroller {
     System.out.println("Hello..");
       return new student("neema", "patel");
   }
-      
+  
+	@GetMapping("/students")
+	public List<student> getStudents(){		
+		List <student> students = new ArrayList<>();
+		students.add(new student("Ramesh", "Fadatare"));
+		students.add(new student("Tony", "Cena"));
+		students.add(new student("Sanjay", "Jadhav"));
+		students.add(new student("Ram", "Jadhav"));
+		students.add(new student("Umesh", "Fadatare"));
+		return students;
+	}
 }
+      
+
